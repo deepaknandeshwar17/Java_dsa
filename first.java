070d47c4cf32801a1858feeef345798a473e6e23
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2024-11-09 22:42:25 +0530

    upadating

--- first.java
+++ first.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class first{
-    public static void main(String[] args){
-        System.out.println("Hello");
+
+    // Functions
+
+    // public static int calculateSum(int a, int b){
+    //     int sum = a + b;
+    //     return sum;
+    // }
+
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     int sum = calculateSum(a, b);
+    //     System.out.println("Sum of the two numbers is: " + sum);
+    // }
+
+
+
+
+    // Factorial of a number
+    public static void factNumb(int n){
+        if (n < 0){
+            System.out.println("Invalid Input");
+            return;
+        }
+        int factorial = 1;
+        for(int i = n; i>=1; i--`){
+            factorial*=i;
+        }
+        System.out.println(factorial);
+        return;
     }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        factNumb(n);
+    }
+
 }
+
